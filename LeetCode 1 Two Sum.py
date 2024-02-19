c2093d7e913051b11701d31ffdc77b2d21784a9c
IsaacAsante <isaac.editor16@gmail.com>
2024-02-19 23:19:15 +0800

    Two Sum solution added.

---
+++ LeetCode 1 Two Sum.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        tracker = defaultdict(int)
+        # Use to store correct indices
+        first_index = second_index = -1
+
+        for index, val in enumerate(nums):
+            if (target - val) in tracker:
+                # Save the other index
+                first_index = tracker[target - val]
+                # Save current index
+                second_index = index
+                # Exit immediately
+                break
+            # Otherwise, update normally
+            tracker[val] = index
+        return [first_index, second_index]
